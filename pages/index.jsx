c3b5c2c6c803bd7979fe2ebf1e63191c8fad4988
Victor Lawrence <victorlawrencecode@gmail.com>
2022-08-15 17:42:24 -0400

    Kaboom

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,7 +1,12 @@
 function Homepage(){
   return (
     <div>
-      We welcome you to the homepage of <em>To Whom Does This Line Belong, Anyhow?</em>, an improvised comedy entertainment.
+      <div>
+        We welcome you to the homepage of <em>To Whom Does This Line Belong, Anyhow?</em>, an improvised comedy entertainment.
+      </div>
+      <div>
+        Please return at a later date, we have nothing for you to see here at the moment.
+      </div>
     </div>
   )
 }
